2d465a894598191319dac4f93c924904fbbc0328
Kett1eC0rn <vegastristan1@gmail.com>
2022-05-17 12:42:34 +0800

    DatabaseSeeder Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call([
-            PostTableSeeder::class,
-        ]);
+        $faker = Faker::create();
+        foreach(range(1,10) as $index){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('secret')
+            ]);
+        }
+
+        
+        // $this->call([
+        //     PostTableSeeder::class,
+        // ]);
     }
 }
